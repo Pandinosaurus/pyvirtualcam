6978ce6c6c2b78b171401b56e90e0d7c93c80f67
Maik Riechert <maik.riechert@arcor.de>
2021-05-03 09:33:49 +0100

    remove unused vs init we don't have external dependencies built outside setuptools

--- .github/scripts/build-windows.ps1
+++ .github/scripts/build-windows.ps1
@@ -17,64 +17,6 @@ function exec {
     }
 }
 
-function Init-VS {
-    # https://wiki.python.org/moin/WindowsCompilers
-    # setuptools automatically selects the right compiler for building
-    # the extension module. The following is mostly for building any
-    # dependencies like libraw.
-    # https://docs.microsoft.com/en-us/cpp/build/building-on-the-command-line
-    # https://docs.microsoft.com/en-us/cpp/porting/binary-compat-2015-2017
-
-    $VS2017_ROOT = "C:\Program Files (x86)\Microsoft Visual Studio\2017"
-    $VS2019_ROOT = "C:\Program Files (x86)\Microsoft Visual Studio\2019"
-
-    $VS_ARCH = if ($env:PYTHON_ARCH -eq '32') { 'x86' } else { 'x64' }
-
-    $PYTHON_VERSION_TUPLE = $env:PYTHON_VERSION.split('.')
-    $PYTHON_VERSION_MAJOR = $PYTHON_VERSION_TUPLE[0]
-    $PYTHON_VERSION_MINOR = $PYTHON_VERSION_TUPLE[1]
-
-    if ($PYTHON_VERSION_MAJOR -eq '3') {
-        if ($PYTHON_VERSION_MINOR -le '4') {
-            throw ("Python <= 3.4 unsupported: $env:PYTHON_VERSION")
-        }
-        if (exists $VS2017_ROOT) {
-            $VS_VERSION = "2017"
-            if (exists "$VS2017_ROOT\Enterprise") {
-                $VS_ROOT = "$VS2017_ROOT\Enterprise"
-            } else {
-                $VS_ROOT = "$VS2017_ROOT\Community"
-            }
-            $VS_INIT_CMD = "$VS_ROOT\Common7\Tools\vsdevcmd.bat"
-            $VS_INIT_ARGS = "-arch=$VS_ARCH -no_logo"
-        } elseif (exists $VS2019_ROOT) {
-            $VS_VERSION = "2019"
-            if (exists "$VS2019_ROOT\Enterprise") {
-                $VS_ROOT = "$VS2019_ROOT\Enterprise"
-            } else {
-                $VS_ROOT = "$VS2019_ROOT\Community"
-            }
-            $VS_INIT_CMD = "$VS_ROOT\Common7\Tools\vsdevcmd.bat"
-            $VS_INIT_ARGS = "-arch=$VS_ARCH -no_logo"
-        } else {
-            throw ("No suitable Visual Studio installation found")
-        }
-    } else {
-        throw ("Unsupported Python version: $PYTHON_VERSION_MAJOR")
-    }
-    Write-Host "Configuring VS$VS_VERSION for Python $env:PYTHON_VERSION on a $env:PYTHON_ARCH bit architecture"
-    Write-Host "Executing: $VS_INIT_CMD $VS_INIT_ARGS"
-
-    # https://github.com/Microsoft/vswhere/wiki/Start-Developer-Command-Prompt
-    & "${env:COMSPEC}" /s /c "`"$VS_INIT_CMD`" $VS_INIT_ARGS && set" | foreach-object {
-        $name, $value = $_ -split '=', 2
-        try {
-            set-content env:\"$name" $value
-        } catch {
-        }
-    }
-}
-
 function HasCondaEnv($name) {
     $env_base = conda info --base
     $env_dir = "$env_base/envs/$name"
@@ -90,8 +32,6 @@ if ($env:PYTHON_ARCH -ne '32' -and $env:PYTHON_ARCH -ne '64') {
 if (!$env:NUMPY_VERSION) {
     throw "NUMPY_VERSION env var missing"
 }
-
-Init-VS
 
 Get-ChildItem env:
 
